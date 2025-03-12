57afacbde6376c33fe406d8690bebd2ff7033cc0
JacobWick <lekonhre@gmail.com>
2025-03-12 15:09:04 +0100

    nugets for entity framework and identity

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities;
+
+public class User : IdentityUser
+{
+    public string Nickname { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    
+}
