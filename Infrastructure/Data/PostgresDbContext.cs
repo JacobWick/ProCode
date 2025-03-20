e19f442a8ae84de57b44ccf6c7fecf936761a5d9
JacobWick <lekonhre@gmail.com>
2025-03-20 17:27:33 +0100

    naming corrections

--- Infrastructure/Data/PostgresDbContext.cs
+++ Infrastructure/Data/PostgresDbContext.cs
@@ -5,18 +5,16 @@ namespace Infrastructure.Data
 {
     public class PostgresDbContext: DbContext
     {
-        public DbSet<Course> courses { get; set; }
-        public DbSet<Exercise> exercises { get; set; }
-        public DbSet<Lesson> lessons { get; set; }
-        public DbSet<Notification> notifications { get; set; }
-        public DbSet<Progress> progresses { get; set; }
-        public DbSet<SolutionExample> solutionExamples { get; set; }
-        public DbSet<Tests> tests { get; set; }
-        public DbSet<User> users { get; set; }
-        public DbSet<ValidationMethod> validationMethods { get; set; }
-
-
-
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Exercise> Exercises { get; set; }
+        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Progress> Progresses { get; set; }
+        public DbSet<SolutionExample> SolutionExamples { get; set; }
+        public DbSet<Tests> Tests { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<ValidationMethod> ValidationMethods { get; set; }
+        
         public PostgresDbContext(DbContextOptions options) : base(options)
         {
         }
