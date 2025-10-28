2c174f68bc827f6750dcc642cea7f431bccc2dca
jjkrz <jj.krzewski@gmail.com>
2025-10-28 09:42:17 +0100

    Add routing and change route for exercise page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
-import {Box} from "@chakra-ui/react";
-import CodeEditor from "./components/CodeEditor.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ExercisePage from "./pages/ExercisePage";
 
 function App() {
   return (
-   <Box minH="100vh" bg='#0f0a19' color='gray.500' px={6} py={8}>
-        <CodeEditor/>
-   </Box>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/exercise" element={< ExercisePage/>} /> // will be changed later to dynamic route
+      </Routes>
+    </BrowserRouter>
   );
 }
 
