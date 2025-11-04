da0d16af74153722d010a8e43eb02a171f3c585a
jjkrz <jj.krzewski@gmail.com>
2025-11-04 10:06:25 +0100

    Home page routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import ExercisePage from "./pages/ExercisePage";
+import HomePage from "./pages/HomePage";
+import './index.css'
 
 function App() {
   return (
-    <BrowserRouter>
       <Routes>
-        <Route path="/exercise" element={< ExercisePage/>} /> // will be changed later to dynamic route
+        <Route path="/exercise" element={< ExercisePage/>} /> {/* will be changed later to dynamic route*/}
+        <Route path="/" element={< HomePage/>} />
       </Routes>
-    </BrowserRouter>
   );
 }
 
