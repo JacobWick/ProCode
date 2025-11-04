c28b542a8fee97a8de32e981f797c993763301b5
jjkrz <jj.krzewski@gmail.com>
2025-11-04 10:07:48 +0100

    Add home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import theme from './theme'
 import App from './App.jsx'
 import {ChakraProvider} from "@chakra-ui/react";
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
   <ChakraProvider theme={theme}>
+    <BrowserRouter>
       <App></App>
+    </BrowserRouter>
   </ChakraProvider>
   </StrictMode>,
 )
