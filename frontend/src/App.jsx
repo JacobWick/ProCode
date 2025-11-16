2b4dd221fc5e54fa7343d65691f663c1e4f534c2
jjkrz <jj.krzewski@gmail.com>
2025-11-16 16:15:18 +0100

    Add navigations in navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,16 @@ import './index.css'
 import CoursesPage from "./pages/CoursesPage.jsx";
 import CourseDetailPage from "./pages/CourseDetailPage.jsx";
 import LessonPage from "./pages/LessonPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage.jsx";
 
 function App() {
     return (
             <Routes>
                 <Route path="/" element={<HomePage/>} />
                 <Route path="/exercise" element={<ExercisePage/>} />
+                <Route path="/login" element={<LoginPage/>} />
+                <Route path="/register" element={<RegisterPage/>} />
                 <Route path="/courses" element={<CoursesPage/>} />
                 <Route path="/courses/:id" element={<CourseDetailPage/>} />
                 <Route path="/courses/:courseId/lessons/:lessonId" element={<LessonPage />} />
