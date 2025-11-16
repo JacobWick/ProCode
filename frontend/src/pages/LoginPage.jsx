6308d2988da1ad2cb3680ec4c56556bb38820ac1
jjkrz <jj.krzewski@gmail.com>
2025-11-16 16:14:39 +0100

    Add functional login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,36 +1,99 @@
-import { Box, Input, Container, FormControl, Heading, Button, FormLabel, VStack, InputGroup, InputRightElement, IconButton } from "@chakra-ui/react"
+import { Box, Input, Container, FormControl, Heading, Button, FormLabel, VStack, InputGroup, InputRightElement, IconButton, Text, Link } from "@chakra-ui/react";
 import { useState } from "react";
-import { ViewIcon } from "@chakra-ui/icons";
-
+import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
+import { Link as RouterLink } from "react-router-dom";
+import { login } from "../api.js"
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+    const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
 
-    return (
-        <Box minH="100vh">
-            <Container>
-                <Box>
-                    <Heading>Witamy ponownie w ProCode!</Heading>
-                    <VStack>
-                        <FormControl>
-                            <InputGroup>
-                            <Input placeholder="Nazwa użytkownika"></Input>
-                            </InputGroup>
+    const {
+        register,
+        handleSubmit,
+    } = useForm();
 
-                            <InputGroup>
-                                <Input type={showPassword ? "text":"password"} placeholder="Hasło"/>
-                                <InputRightElement>
-                                    <IconButton onClick={() => setShowPassword(!showPassword)} icon={<ViewIcon/>}/>
-                                </InputRightElement>
-                            </InputGroup>
+  const handleLogin = async (data) => {
+    try {    
+        const res = await login(data)
 
-                            <Button>Zaloguj się</Button>
-                        </FormControl>
-                    </VStack>
-                </Box>
-            </Container>
+        localStorage.setItem("token", res.data.token);
+        navigate("/");
+    } 
+    catch (err) {
+        console.error(err);
+        alert("Niepoprawne dane logowania");
+    }
+
+  }
+
+  return (
+    <Box minH="100vh" display="flex" alignItems="center" justifyContent="center" bg="purple.50" p={4}>
+      <Container maxW="md">
+        <Box bg="white" p={8} rounded="2xl" boxShadow="xl">
+          <Heading textAlign="center" mb={6} fontSize="2xl">
+            Witamy ponownie w <Text size="md" color="purple.500" >ProCode!</Text>
+          </Heading>
+
+          <form onSubmit={handleSubmit(handleLogin)}>
+          <VStack spacing={5}>
+            <FormControl>
+              <FormLabel>Nazwa użytkownika</FormLabel>
+              <InputGroup>
+                <Input placeholder="Wpisz nazwę użytkownika"
+                    bg="gray.50"
+                    p={6}
+                    rounded="xl"
+                    {...register("userName")}
+                />
+              </InputGroup>
+            </FormControl>
+
+            <FormControl>
+              <FormLabel>Hasło</FormLabel>
+              <InputGroup>
+                <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Wpisz hasło"
+                    bg="gray.50"
+                    p={6}
+                    rounded="xl"
+                    {...register("password")}
+                />
+                <InputRightElement h="full" rounded="xl">
+                    <IconButton
+                        variant="ghost"
+                        aria-label="Pokaż hasło"
+                        onClick={() => setShowPassword(!showPassword)}
+                        icon={showPassword? <ViewOffIcon/> : <ViewIcon />}
+                        h="full"
+                        rounded="xl"
+                    />
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+
+            <Button
+                type="submit"
+                colorScheme="blue"
+                width="full"
+                size="lg"
+                rounded="xl"
+                fontWeight="bold"
+            >
+              Zaloguj się
+            </Button>
+
+            <Text>Nie masz jeszcze konta?<Link color="blue.500" as={RouterLink} to="/register"> Zarejestruj się</Link></Text>
+            
+          </VStack>
+          </form>
         </Box>
-    )
-}
+      </Container>
+    </Box>
+  );
+};
 
 export default LoginPage;
