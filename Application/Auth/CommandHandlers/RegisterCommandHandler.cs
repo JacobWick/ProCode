7aab219455a1b1d5960ebc1bdfe01192f14a40b0
jjkrz <jj.krzewski@gmail.com>
2025-11-17 15:58:08 +0100

    Add profile assignment on registration

--- Application/Auth/CommandHandlers/RegisterCommandHandler.cs
+++ Application/Auth/CommandHandlers/RegisterCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Auth.Commands;
+using Application.Interfaces;
 using Domain.Constants;
 using Domain.Entities;
 using Domain.Exceptions;
@@ -10,10 +11,12 @@ namespace Application.Auth.CommandHandlers
     public class RegisterCommandHandler : IRequestHandler<RegisterCommand, bool>
     {
         private readonly UserManager<User> _userManager;
+        private readonly IRepository<UserProfile> _profileRepo;
 
-        public RegisterCommandHandler(UserManager<User> userManager)
+        public RegisterCommandHandler(UserManager<User> userManager, IRepository<UserProfile> profileRepo)
         {
             _userManager = userManager;
+            _profileRepo = profileRepo;
         }
 
         public async Task<bool> Handle(RegisterCommand request, CancellationToken cancellationToken)
@@ -34,6 +37,12 @@ namespace Application.Auth.CommandHandlers
             var result = await _userManager.CreateAsync(user, request.Password);
 
             await _userManager.AddToRoleAsync(user, Roles.Student);
+
+            var profile = new UserProfile { 
+                UserId = user.Id 
+            };
+
+            await _profileRepo.CreateAsync(profile, cancellationToken);
 
             return result.Succeeded;
         }
