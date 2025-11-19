0c2761c5be5c143e3ba4c41e9538f1e4f491889a
jjkrz <jj.krzewski@gmail.com>
2025-11-20 00:53:21 +0100

    Add personalized paths query, handler and endpoint

---
+++ Application/Courses/QueriesHandlers/GetPersonalizedRoadmapsQueryHandler.cs
@@ -0,0 +1,55 @@
+﻿using Application.Courses.Queries;
+using Application.DTOs;
+using Application.Interfaces;
+using Domain.Entities;
+using MediatR;
+using System.Linq.Expressions;
+
+namespace Application.Courses.QueriesHandlers
+{
+    public class GetPersonalizedRoadmapsQueryHandler : IRequestHandler<GetPersonalizedRoadmapsQuery, RoadmapDto>
+    {
+        private readonly IRepository<Course> _courseRepo;
+        private readonly IRepository<User> _userRepo;
+        private readonly IPersonalizationService _personalizationService;
+        private readonly IUserContextService _userContext;
+
+        public GetPersonalizedRoadmapsQueryHandler(
+            IRepository<Course> courseRepo, 
+            IRepository<User> userRepo, 
+            IPersonalizationService personalizationService,
+            IUserContextService userContext)
+        {
+            _courseRepo = courseRepo;
+            _userRepo = userRepo;
+            _personalizationService = personalizationService;
+            _userContext = userContext;
+        }
+
+        public async Task<RoadmapDto> Handle(GetPersonalizedRoadmapsQuery request, CancellationToken cancellationToken)
+        {
+            var userId = _userContext.UserId;
+
+            if (userId == Guid.Empty)
+                throw new UnauthorizedAccessException("You are not authorized to access this resource.");
+
+            var user = await _userRepo.GetByIdAsync(
+                userId,
+                includes: new Expression<Func<User, object>>[]
+                {
+                    u => u.Progresses,
+                    u => u.TagsIntrestedIn,
+                },
+                cancellationToken: cancellationToken
+            );
+
+            if (user is null)
+                throw new Exception("User not found");
+
+            var allCourses = await _courseRepo.GetAllAsync(cancellationToken: cancellationToken); // pobieramy wszystko
+            var roadmap = _personalizationService.GenerateRoadmap(user, allCourses);
+
+            return roadmap;
+        }
+    }
+}
---
+++ Application/DTOs/RoadmapDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.DTOs
+{
+    public class RoadmapDto
+    {
+        public Guid UserId { get; set; }
+        public List<CourseDto> RecommendedCourses { get; set; } = new();
+    }
+}
